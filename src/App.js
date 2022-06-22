5cb4711017238aa2824f28e30c1f90948c2d2bfc
minkyeongJ <jomk2221@naver.com>
2022-06-22 23:53:44 +0900

    Feat: App.js index.js 추가

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import GlobalStyle from './components/GlobalStyle';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+	return (
+  <>
+    <GlobalStyle />
+    <Header />
+    <Banner />
+    <Main />
+    <Footer />
+  </>
+  );
+}
+
+export default App;
