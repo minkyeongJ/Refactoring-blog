c1bc44b58675ceceb6b299d1c008b24644848ce3
minkyeongJ <jomk2221@naver.com>
2022-06-27 23:52:31 +0900

    Feat: 블로그 메인 베너 동적으로 가져오는 기능 추가

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from 'react';
 import './css/Banner.css';
 
 function MainBanner(){
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data.json", {
+      method: 'GET',
+    })
+    .then(res => res.json())
+    .then(data => {
+      setData(data);
+    });
+  }, []);
+
+  const bannerData = data && data.blog;
+  const mainTitle = bannerData && bannerData.mainTitle;
+  const subTitle = bannerData && bannerData.subTitle;
+  const description = bannerData && bannerData.description;
+
   return (
     <div className="banner">
 			<div className="max-width">
 				<div className="banner-contents">
-					<p className="sub-text">React Blog</p>
-					<p className="main-text">My BLOG</p>
+					<p className="sub-text">{subTitle}</p>
+					<p className="main-text">{mainTitle}</p>
 					<p className="description">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nostrum dolor provident beatae deserunt magnam, illo neque. Maiores ipsum commodi dolorum beatae aliquid laboriosam est
+						{description}
 					</p>
 				</div>
 			</div>
