9efb19997cc355358e3e839348754234ddc449b4
minkyeongJ <jomk2221@naver.com>
2022-10-24 23:49:55 +0900

    Fix: url 경로 변경

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/refactoring-blog.vercel.app">
         <GlobalStyle />
         <Header />
         <Router />
